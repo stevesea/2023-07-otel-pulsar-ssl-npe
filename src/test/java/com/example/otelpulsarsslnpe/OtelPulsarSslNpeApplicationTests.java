bb8bad5c1e085eb484c8a81deba4fdfcab15db7a
steve christensen <schristensen@overstock.com>
2023-07-26 10:36:41 -0600

    initial app from app initializer

---
+++ src/test/java/com/example/otelpulsarsslnpe/OtelPulsarSslNpeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.otelpulsarsslnpe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtelPulsarSslNpeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
